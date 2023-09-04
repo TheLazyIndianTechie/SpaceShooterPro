371f08225988dfc31c84c1f9d8b1e4324f505fb7
Vinay Vidyasagar <86476092+TheLazyIndianTechie@users.noreply.github.com>
2023-09-04 17:42:37 +0530

    Extracted player movement into a new method

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,27 +19,31 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        CalculateMovement();
+    }
+
+    private void CalculateMovement()
+    {
         //Get both horizontal and vertical input per frame 
         float horizontalInput = Input.GetAxis("Horizontal"), verticalInput = Input.GetAxis("Vertical");
-        
+
         //Cache the Vector3 input within update for reuse
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
-        
+
         //Move the player by taking the cached horizontal and vertical input per frame in
         //the x and y directions multiplied by time and speed
         Transform playerTransform;
         (playerTransform = transform).Translate(direction * (Time.deltaTime * speed));
-        
+
         //Create a local cached variable of player's position
         Vector3 playerPosition = playerTransform.position;
-        
+
         //Check and limit player's vertical movement
-        if (playerPosition.y >= 0) playerTransform.position = new Vector3(transform.position.x,0,0);
-        else if (transform.position.y <= - 3.8f) playerTransform.position = new Vector3(transform.position.x, -3.8f, 0);
-        
+        if (playerPosition.y >= 0) playerTransform.position = new Vector3(transform.position.x, 0, 0);
+        else if (transform.position.y <= -3.8f) playerTransform.position = new Vector3(transform.position.x, -3.8f, 0);
+
         //Check and overlap player's horizontal movement
         if (playerPosition.x >= 12f) playerTransform.position = new Vector3(-12, transform.position.y, 0);
         else if (transform.position.x <= -12f) playerTransform.position = new Vector3(12f, transform.position.y, 0);
-        
     }
 }
